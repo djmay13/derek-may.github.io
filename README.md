11003b71b5bde82ddea7d36f270154bc9b5fc038
Derek May <djmay13@outlook.com>
2022-07-04 17:00:30 +0000

    recursion

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# derek-may.github.io
+Install New Projects 
+1)New terminal 
+2)code ~/.bashrc 
+3)On line 4 of bashrc file: export CHE_PROJECTS_ROOT=/workspace 
+4)Close bashrc file 
+5)Close terminal 
+6)Reopen terminal 
+7)sudo apt-get install subversion 
+8)npm install -g opspark 
+9)os install
+
+Test and Submit Projects 1)New terminal 2)code ~/.bashrc 3)On line 4 of bashrc file: export CHE_PROJECTS_ROOT=/workspace 4)Close bashrc file 5)Close terminal 6)Reopen terminal 7)sudo apt-get install subversion 8)npm install -g opspark 9)os test 10)os submit
+
+Github Commands 1)git add -A 2)git commit -m "description" 3)git push
+
+Lodown 1)Go to 3 vertically aligned lines => File => Open File 2)In search bar delete everything except /workspace/ 3)Select lodown below 4)Select index.js
+
+Lodown Github Commands 1)cd .. 2)cd lodown 3)run Github Commands
+
+Test Let's Get Functional 1)line 6 => var _ = require('underbar'); 2)Replace <YOUR GITHUB FOLDER on line 19 with the name of your github folder as it appears in the top left of your gitpod space> 3)cd .. until gitpod /workspace 4)copy and paste line 19
---
+++ projects/recursion-practice/src/recursion.js
@@ -0,0 +1,663 @@
+// Solve all of the following prompts using recursion.
+
+// 1. Calculate the factorial of a number.  The factorial of a non-negative integer n,
+// denoted by n!, is the product of all positive integers less than or equal to n.
+// Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
+// factorial(5);  // 120
+var factorial = function(n) {
+//base
+//determine if n is equal to 0
+  if(!n){
+//if true return 1
+    return 1;
+  }else if (n < 0){
+    return null;
+}
+//recursion
+  return n * factorial(n-1);
+};
+
+// 2. Compute the sum of an array of integers.
+// Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
+var sum = function(array, result = 0) {
+  if(!array.length){
+    return result;
+  }
+  result += array[0];
+  return sum(array.slice(1), result)
+};
+
+// 3. Sum all numbers in an array containing nested arrays.
+// Example: arraySum([1,[2,3],[[4]],5]); // 15
+var arraySum = function(array) {
+};
+
+// 4. Check if a number is even.
+var isEven = function(n) {
+//base
+if(!n){
+  return true;
+}else if(n === 1){
+  return false;
+//recursion
+}if(n < 0){
+  return isEven(-n)
+}if(n > 0){
+  return isEven(n - 2)
+}
+};
+
+// 5. Sum all integers below a given integer.
+// sumBelow(10); // 45
+// sumBelow(7); // 21
+var sumBelow = function(n, sum = 0) {
+//base
+//determine if n is equal to 0
+if(!n){
+//if true return the sum
+  return sum;
+};
+//recursion
+//determine if n is negative
+if(n < 0){
+//if true 
+  sum += n + 1
+  return sumBelow(n+1, sum)
+}
+else{
+sum += n - 1
+return sumBelow(n-1, sum)
+}
+};
+
+// 6. Get the integers in range (x, y).
+// Example:  range(2, 9);  // [3, 4, 5, 6, 7, 8]
+var range = function(x, y, output=[]) {
+    // base
+    if(x === y){
+      return output;
+    }if((x + 1) < y){
+      output.push(x + 1)
+    }if((x - 1) > y) {
+      output.push(x - 1)
+    //recursion
+    }if(x < y) {
+      return range(x + 1, y, output);
+    } 
+    if(x > y) {
+      return range(x - 1, y , output);
+    }
+  };
+
+// 7. Compute the exponent of a number.
+// The exponent of a number says how many times the base number is used as a factor.
+// 8^2 = 8 x 8 = 64.  Here, 8 is the base and 2 is the exponent.
+// Example:  exponent(4,3);  // 64
+// https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
+var exponent = function(base, exp) {
+//base
+//deteermine if exponent is equal to 0
+if(exp === 0){
+//if true result equals 1
+  return 1;
+}
+//recursion
+//determine if exp is positive
+if(exp > 0){
+//if true, multiply base by itself exp amount of times
+  return base * exponent(base, exp - 1)
+//determine if exp is negative
+}else if(exp < 0){
+//if true, divide base by itself exp amount of times
+  return exponent(base, exp + 1) / base
+}
+};
+
+// 8. Determine if a number is a power of two.
+// powerOfTwo(1); // true
+// powerOfTwo(16); // true
+// powerOfTwo(10); // false
+var powerOfTwo = function(n) {
+//base
+  if(n === 2 && n % 2 === 0 || n === 1){
+    return true;
+  }else if (n % 2 !== 0 || n === 0){
+    return false;
+  }
+    //recursion
+  return powerOfTwo(n / 2);
+};
+
+// 9. Write a function that accepts a string a reverses it.
+var reverse = function(string, str) {
+//base
+//
+  if(string === ''){
+    return ''
+  }
+//recursion
+  return reverse(string.substr(1)) + string.charAt(0);
+};
+
+// 10. Write a function that determines if a string is a palindrome.
+var palindrome = function(string) {
+  //base
+  if (string.length === 0){
+    return true;
+  }
+//recursion
+  if (string[0].toLowerCase() === string[string.length - 1].toLowerCase()){
+    return palindrome(string.slice(1).slice(0, -1));
+  }else if (string[0] === ' '){
+    return palindrome(string.slice(1));
+  }else if (string[string.length - 1] === ' '){
+    return palindrome(string.slice(1));
+  }else 
+  return false;
+};
+
+// 11. Write a function that returns the remainder of x divided by y without using the
+// modulo (%) operator.
+// modulo(5,2) // 1
+// modulo(17,5) // 2
+// modulo(22,6) // 4
+var modulo = function(x, y) {
+};
+
+// 12. Write a function that multiplies two numbers without using the * operator  or
+// JavaScript's Math object.
+// ATTENTION DO NOT LEAVE COMMENTS IN THIS FUNCTION. The test is looking for any ('/').
+var multiply = function(x, y) {
+  if(y === 0){
+    return 0;
+  }
+  if(y < 0){
+    return -x + multiply(x, y + 1);
+  }else{
+    return x + multiply(x, y - 1);
+  }
+};
+
+// 13. Write a function that divides two numbers without using the / operator  or
+// JavaScript's Math object.
+var divide = function(x, y) {
+};
+
+// 14. Find the greatest common divisor (gcd) of two positive numbers.  The GCD of two
+// integers is the greatest integer that divides both x and y with no remainder.
+// Example:  gcd(4,36);  // 4
+// http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
+// https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
+var gcd = function(x, y) {
+};
+
+// 15. Write a function that compares each character of two strings and returns true if
+// both are identical.
+// compareStr('house', 'houses') // false
+// compareStr('', '') // true
+// compareStr('tomato', 'tomato') // true
+var compareStr = function(str1, str2){
+// base
+//determine if str1 is equal to str2 
+  if(!str1.length && !str2.length){
+    return true;
+  }
+  // recursion
+  //determine if current element in strs are equal 
+  if (str1[0] === str2[0]){
+  //call function slicing at first element of each str 
+    return compareStr(str1.slice(1), str2.slice(1));
+  }
+  return false;
+};
+
+// 16. Write a function that accepts a string and creates an array where each letter
+// occupies an index of the array.
+var createArray = function(str, output = []){
+//base
+//determine if str has been iterated through
+if(!str.length){
+//if true return output
+  return output;
+}
+//push first element of str into output 
+output.push(str[0])
+//call createArray, slicing the first element each call
+return createArray(str.slice(1), output)
+};
+
+// 17. Reverse the order of an array
+var reverseArr = function (array){
+// base
+//determine if array has been iterated through
+  if(!array.length){
+//if true, return array
+    return array;
+  }
+//recurse through array 
+return reverseArr(array.slice(1)).concat(array[0]);
+}
+
+// 18. Create a new array with a given value and length.
+// buildList(0,5) // [0,0,0,0,0]
+// buildList(7,3) // [7,7,7]
+var buildList = function(value, length, output = []) {
+//base
+//determine if length is 0
+  if(!length){
+//if true, return output
+    return output;
+  }
+//recursion
+//push value into output
+output.push(value)
+//recursivly subtract 1 from length 
+  return buildList(value, length - 1, output)
+};
+
+// 19. Count the occurence of a value inside a list.
+// countOccurrence([2,7,4,4,1,4], 4) // 3
+// countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
+var countOccurrence = function(array, value, count = 0) {
+//base
+//determine if array has been iterated through
+  if(!array.length){
+//if true, return count
+    return count;
+  }
+//recursion
+//determine if element at 0 index of array is equal to value
+if(array[0] === value){
+//if true increment count 
+  count ++
+}
+//recurse through array 
+return countOccurrence(array.slice(1), value, count)
+};
+
+// 20. Write a recursive version of map.
+// rMap([1,2,3], timesTwo); // [2,4,6]
+var rMap = function(array, callback, output = []) {
+// base
+//determine if array has been iterated through
+  if (!array.length){
+//if true return output
+    return output;
+  }
+//recursion
+//push result of callback function on element at 0 index of array into output
+  output.push(callback(array[0]));
+//recurse through array
+  return rMap(array.slice(1), callback, output)
+};
+
+// 21. Write a function that counts the number of times a key occurs in an object.
+// var testobj = {'e': {'x':'y'}, 't':{'r': {'e':'r'}, 'p': {'y':'r'}},'y':'e'};
+// countKeysInObj(testobj, 'r') // 1
+// countKeysInObj(testobj, 'e') // 2
+var countKeysInObj = function(obj, key) {
+};
+
+// 22. Write a function that counts the number of times a value occurs in an object.
+// var testobj = {'e': {'x':'y'}, 't':{'r': {'e':'r'}, 'p': {'y':'r'}},'y':'e'};
+// countValuesInObj(testobj, 'r') // 2
+// countValuesInObj(testobj, 'e') // 1
+var countValuesInObj = function(obj, value) {
+};
+
+// 23. Find all keys in an object (and nested objects) by a provided name and rename
+// them to a provided new name while preserving the value stored at that key.
+var replaceKeysInObj = function(obj, key, newKey) {
+};
+
+// 24. Get the first n Fibonacci numbers.  In the Fibonacci Sequence, each subsequent
+// number is the sum of the previous two.
+// Example:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34.....
+// fibonacci(5);  // [0, 1, 1, 2, 3, 5]
+// Note:  The 0 is not counted.
+var fibonacci = function(n) {
+};
+
+// 25. Return the Fibonacci number located at index n of the Fibonacci sequence.
+// [0,1,1,2,3,5,8,13,21]
+// nthFibo(5); // 5
+// nthFibo(7); // 13
+// nthFibo(3); // 2
+var nthFibo = function(n, num = 0) {
+//base
+//determine if n is negative
+  if(n < 0){
+//if true return null
+    return null;
+//detrermine if n === 1
+  }if(n === 1){
+//if true return n
+    return n;
+  }
+//recursion
+//recursivly iterate subtracting from n
+  return nthFibo(n - 2) + nthFibo(n - 1);
+};
+// 26. Given an array of words, return a new array containing each word capitalized.
+// var words = ['i', 'am', 'learning', 'recursion'];
+// capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
+var capitalizeWords = function(input, output =[]) {
+//base
+  if(!input.length){
+    return output;
+   }
+//recursion
+//push element at 0 index of input capitalized
+  output.push(input[0].toUpperCase())
+//recursivly iterate through input
+    return capitalizeWords(input.slice(1), output);
+};
+
+// 27. Given an array of strings, capitalize the first letter of each index.
+// capitalizeFirst(['car', 'poop', 'banana']); // ['Car', 'Poop', 'Banana']
+var capitalizeFirst = function(array, output = []) {
+//base
+//determine if array has been iterated through
+  if(!array.length){
+//if true return output
+    return output;
+  }
+//recursion
+//push element at 0 index into output capitalized at 0 index
+  output.push(array[0][0].toUpperCase() + array[0].slice(1));
+  return capitalizeFirst(array.slice(1), output);
+};
+
+// 28. Return the sum of all even numbers in an object containing nested objects.
+// var obj1 = {
+//   a: 2,
+//   b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+//   c: {c: {c: 2}, cc: 'ball', ccc: 5},
+//   d: 1,
+//   e: {e: {e: 2}, ee: 'car'}
+// };
+// nestedEvenSum(obj1); // 10
+var nestedEvenSum = function(obj) {
+};
+
+// 29. Flatten an array containing nested arrays.
+// Example: flatten([1,[2],[3,[[4]]],5]); // [1,2,3,4,5]
+var flatten = function(arrays) {
+};
+
+// 30. Given a string, return an object containing tallies of each letter.
+// letterTally('potato'); // {'p':1, 'o':2, 't':2, 'a':1}
+var letterTally = function(str, output = {}){
+//base
+//determine if str has been iterated through
+  if(!str.length){
+//if true, return output
+    return output;
+//recursion
+//detrmine if output does not contain char at 0 index of str
+  }if(!output[str[0]]){
+//if true, make key of char at 0 index of string have a value equal to 1
+    output[str[0]] = 1;
+//if output does contain char at 0 index str, increment value
+  }else{
+    output[str[0]] ++;
+  }
+  return letterTally(str.slice(1), output);   
+}; 
+
+// 31. Eliminate consecutive duplicates in a list.  If the list contains repeated
+// elements they should be replaced with a single copy of the element. The order of the
+// elements should not be changed.
+// Example: compress([1, 2, 2, 3, 4, 4, 5, 5, 5]) // [1, 2, 3, 4, 5]
+// Example: compress([1, 2, 2, 3, 4, 4, 2, 5, 5, 5, 4, 4]) // [1, 2, 3, 4, 2, 5, 4]
+var compress = function(list, output =[]) {
+//base
+//determine if list has been iterated through
+  if(!list.length){
+//if true, return output
+    return output;
+  }
+//recursion
+//detertmine if next element is equal to element at 0 
+//index and element at 0 index of output does not equal element at 0 index of list
+  if(list[0] !== list[1] && output[0] !== list[0]){
+//if true, push element at 0 index of list into output
+    output.push(list[0])
+ }
+ return compress(list.slice(1), output);
+};
+
+// 32. Augment every element in a list with a new value where each element is an array
+// itself.
+// Example: augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
+var augmentElements = function(array, aug, output = []) {
+//determine if array has been iterated through
+  if(!array.length){
+//if true, return output
+    return output
+  }
+//push aug into element at 0 index of array
+  array[0].push(aug);
+//push element at 0 index of array into output
+  output.push(array[0])
+  return augmentElements(array.slice(1), aug, output)
+};
+
+// 33. Reduce a series of zeroes to a single 0.
+// minimizeZeroes([2,0,0,0,1,4]) // [2,0,1,4]
+// minimizeZeroes([2,0,0,0,1,0,0,4]) // [2,0,1,0,4]
+var minimizeZeroes = function(array, output = []) {
+//base
+//determine if array has been iterated through
+  if(!array.length){
+//if true, return output
+    return output;
+ }
+//recursion
+//detertmine if next element is equal to 0 and element 
+//at 0 index of output does not equal element at 0 index of list
+  if(array[0] !== array[1] && array[0] !== output[0]){
+//if true, push element into output
+    output.push(array[0])
+   }
+   return minimizeZeroes(array.slice(1), output);
+ };
+
+// 34. Alternate the numbers in an array between positive and negative regardless of
+// their original sign.  The first number in the index always needs to be positive.
+// alternateSign([2,7,8,3,1,4]) // [2,-7,8,-3,1,-4]
+// alternateSign([-2,-7,8,3,-1,4]) // [2,-7,8,-3,1,-4]
+var alternateSign = function(array, output = []) {
+//base
+//determine if array has been iterated through
+  if(!array.length){
+//if true, return output
+    return output;
+  }
+//recursion
+//determine if element at 0 index of array is negative
+  if(array[0] < 0){
+//if true, multiply by -1 to make positive and push into output
+    output.push(array[0] * -1)
+  }else{
+    output.push(array[0])
+  }
+//determine if next element is positive
+  if(array[1] > 0){
+//if true, multiply by -1 to make negative and push into output
+    output.push(array[1] * -1)
+  }else{
+    output.push(array[1])
+  }
+//iterate through array recursivly by every other element
+  return alternateSign(array.slice(2), output);
+};
+
+// 35. Given a string, return a string with digits converted to their word equivalent.
+// Assume all numbers are single digits (less than 10).
+// numToText("I have 5 dogs and 6 ponies"); // "I have five dogs and six ponies"
+var numToText = function(str, output = '') {
+//base
+//create variable of number words
+  let nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+//determine if str has been iterated through
+  if (!str.length){
+    return output;
+  }
+//recursion
+//determine if element  is a number. and not a space
+  if(Number(str[0]) && str[0] !== ' ') {
+    return numToText(str.slice(1), output += nums[Number(str[0])]);
+  }
+  return numToText(str.slice(1), output += str[0]);
+};
+
+// *** EXTRA CREDIT ***
+
+// 36. Return the number of times a tag occurs in the DOM.
+var tagCount = function(tag, node) {
+};
+
+// 37. Write a function for binary search.
+// Sample array:  [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+// console.log(binarySearch(5)) will return '5'
+
+var binarySearch = function(array, target, min, max) {
+};
+
+// 38. Write a merge sort function.
+// Sample array:  [34,7,23,32,5,62]
+// Sample output: [5,7,23,32,34,62]
+var mergeSort = function(array) {
+};
+
+
+
+//-----------------------------------
+// DON'T REMOVE THIS CODE -----------
+//-----------------------------------
+
+if ((typeof process !== 'undefined') &&
+  (typeof process.versions.node !== 'undefined')) {
+
+  /**
+   * Due to some node-related issues with spying on recursive functions,
+   * it isn't possible to test them with sinon spies like so:
+   *
+   *   var originalSum = sum;
+   *   sum = sinon.spy(sum);
+   *
+   *   sum([1, 2, 3, 4, 5, 6]);
+   *
+   *   // callCount will always 1 causing, this test to always fail in node :(
+   *   expect(sum.callCount).to.be.above(1);
+   *
+   *   sum = originalSum;
+   *
+   * However, we can work around this by using proxies!
+   * If you reassign the function to a proxy and use the `apply` trap,
+   * you can make a `proxyCallCount` property on the function,
+   * increment it each time it's called, and then test that instead.
+   *
+   *   sum.proxyCallCount = 0;
+   *   sum([1, 2, 3, 4, 5, 6]);
+   *   expect(sum.proxyCallCount).to.be.above(1);
+   *
+   * MDN Proxies: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+   * MDN Proxy Apply Trap: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy/handler/apply
+   */
+  const createSpyProxy = (func) => {
+    func.toString = func.toString.bind(func);
+
+    const recursiveFunctionCallCounterHandler = {
+      apply(target, thisArg, args) {
+        target.proxyCallCount = target.proxyCallCount ? target.proxyCallCount + 1 : 1;
+        return target.apply(thisArg, args);
+      },
+    };
+
+    return new Proxy(func, recursiveFunctionCallCounterHandler);
+  };
+
+  factorial = createSpyProxy(factorial);
+  sum = createSpyProxy(sum);
+  arraySum = createSpyProxy(arraySum);
+  isEven = createSpyProxy(isEven);
+  sumBelow = createSpyProxy(sumBelow);
+  range = createSpyProxy(range);
+  exponent = createSpyProxy(exponent);
+  powerOfTwo = createSpyProxy(powerOfTwo);
+  reverse = createSpyProxy(reverse);
+  palindrome = createSpyProxy(palindrome);
+  modulo = createSpyProxy(modulo);
+  multiply = createSpyProxy(multiply);
+  divide = createSpyProxy(divide);
+  gcd = createSpyProxy(gcd);
+  compareStr = createSpyProxy(compareStr);
+  createArray = createSpyProxy(createArray);
+  reverseArr = createSpyProxy(reverseArr);
+  buildList = createSpyProxy(buildList);
+  countOccurrence = createSpyProxy(countOccurrence);
+  rMap = createSpyProxy(rMap);
+  countKeysInObj = createSpyProxy(countKeysInObj);
+  countValuesInObj = createSpyProxy(countValuesInObj);
+  replaceKeysInObj = createSpyProxy(replaceKeysInObj);
+  fibonacci = createSpyProxy(fibonacci);
+  nthFibo = createSpyProxy(nthFibo);
+  capitalizeWords = createSpyProxy(capitalizeWords);
+  capitalizeFirst = createSpyProxy(capitalizeFirst);
+  nestedEvenSum = createSpyProxy(nestedEvenSum);
+  flatten = createSpyProxy(flatten);
+  letterTally = createSpyProxy(letterTally);
+  compress = createSpyProxy(compress);
+  augmentElements = createSpyProxy(augmentElements);
+  minimizeZeroes = createSpyProxy(minimizeZeroes);
+  alternateSign = createSpyProxy(alternateSign);
+  numToText = createSpyProxy(numToText);
+  tagCount = createSpyProxy(tagCount);
+  binarySearch = createSpyProxy(binarySearch);
+  mergeSort = createSpyProxy(mergeSort);
+
+  module.exports = {
+    factorial,
+    sum,
+    arraySum,
+    isEven,
+    sumBelow,
+    range,
+    exponent,
+    powerOfTwo,
+    reverse,
+    palindrome,
+    modulo,
+    multiply,
+    divide,
+    gcd,
+    compareStr,
+    createArray,
+    reverseArr,
+    buildList,
+    countOccurrence,
+    rMap,
+    countKeysInObj,
+    countValuesInObj,
+    replaceKeysInObj,
+    fibonacci,
+    nthFibo,
+    capitalizeWords,
+    capitalizeFirst,
+    nestedEvenSum,
+    flatten,
+    letterTally,
+    compress,
+    augmentElements,
+    minimizeZeroes,
+    alternateSign,
+    numToText,
+    tagCount,
+    binarySearch,
+    mergeSort,
+  };
+}
+
+//-----------------------------------
